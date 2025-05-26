5487dd6df592ca7237414b07bb189d4606aaae75
Mayank singh chauhan <113712051+MAYANKSINGHCHAUHAN@users.noreply.github.com>
2025-05-26 18:09:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './components/store.js'; 
+import App from './components/App.js';
 
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
